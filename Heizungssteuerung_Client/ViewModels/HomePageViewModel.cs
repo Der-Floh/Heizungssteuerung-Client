18cc11d96aa3bcf98e5c4441d299cae52bda8613
Christopher0802 <cguschky@googlemail.com>
2023-11-07 14:30:50 +0100

    Menu

---
+++ Heizungssteuerung_Client/ViewModels/HomePageViewModel.cs
@@ -0,0 +1,23 @@
+﻿namespace Heizungssteuerung_Client.ViewModels;
+
+public class HomePageViewModel : ViewModelBase
+{
+    //private bool _isSplitViewPaneOpen = false;
+    //public bool IsSplitViewPaneOpen
+    //{
+    //    get => _isSplitViewPaneOpen;
+    //    set => this.RaiseAndSetIfChanged(ref _isSplitViewPaneOpen, value);
+    //}
+
+    //private ViewModelBase _currentPage = new HomePageViewModel();
+
+    //public ObservableCollection<ListItemTemplate> Items { get; } = new()
+    //{
+    //    new ListItemTemplate(typeof(HomePageViewModel)),
+    //};
+
+    //public void TriggerPane()
+    //{
+    //    IsSplitViewPaneOpen = !IsSplitViewPaneOpen;
+    //}
+}
--- Heizungssteuerung_Client/ViewModels/MainViewModel.cs
+++ Heizungssteuerung_Client/ViewModels/MainViewModel.cs
@@ -1,6 +1,43 @@
-﻿namespace Heizungssteuerung_Client.ViewModels;
+﻿using ReactiveUI;
+using System;
+using System.Collections.ObjectModel;
+
+namespace Heizungssteuerung_Client.ViewModels;
 
 public class MainViewModel : ViewModelBase
 {
-    public string Greeting => "Welcome to Avalonia!";
+    private bool _isSplitViewPaneOpen = false;
+    public bool IsSplitViewPaneOpen
+    {
+        get => _isSplitViewPaneOpen;
+        set => this.RaiseAndSetIfChanged(ref _isSplitViewPaneOpen, value);
+    }
+
+    private ViewModelBase _currentPage = new HomePageViewModel();
+    public ViewModelBase CurrentPage
+    {
+        get => _currentPage;
+        set => this.RaiseAndSetIfChanged(ref _currentPage, value);
+    }
+
+    public ObservableCollection<ListItemTemplate> Items { get; } = new()
+    {
+        new ListItemTemplate(typeof(HomePageViewModel)),
+    };
+
+    public void TriggerPane()
+    {
+        IsSplitViewPaneOpen = !IsSplitViewPaneOpen;
+    }
+}
+
+public class ListItemTemplate
+{
+    public ListItemTemplate(Type type)
+    {
+        ModelType = type;
+        Label = type.Name.Replace("PageViewModel", "");
+    }
+    public string Label { get; set; }
+    public Type ModelType { get; set; }
 }
