d75238297ac4335a710f7dd895f5aff6f9d1f82f
Der-Floh <derr.floh@gmail.com>
2023-11-08 17:06:36 +0100

    Menu Image

--- Heizungssteuerung_Client/Views/MainView.axaml.cs
+++ Heizungssteuerung_Client/Views/MainView.axaml.cs
@@ -4,28 +4,8 @@ namespace Heizungssteuerung_Client.Views;
 
 public partial class MainView : UserControl
 {
-    //public bool IsSplitViewPaneOpen
-    //{
-    //    get => _isSplitViewPaneOpen;
-    //    set
-    //    {
-    //        _isSplitViewPaneOpen = value;
-    //        SplitViewCom.IsPaneOpen = value;
-    //        if (value)
-    //            TriggerPaneButton.Content = "-";
-    //        else
-    //            TriggerPaneButton.Content = "+";
-    //    }
-    //}
-    //private bool _isSplitViewPaneOpen = false;
-    //public ICommand TriggerPaneCommand { get; set; }
     public MainView()
     {
         InitializeComponent();
-        //TriggerPaneButton.Click += TriggerPaneButton_Click;
     }
-    //private void TriggerPaneButton_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
-    //{
-    //    IsSplitViewPaneOpen = !IsSplitViewPaneOpen;
-    //}
 }
